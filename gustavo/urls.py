a137384d3865f4cf23a4f277f094b3e8ca97fc1e
Gustavo Bisetti <gustavo@tracy.com.br>
2013-08-08 15:10:58 -0300

    galery insert

--- gustavo/urls.py
+++ gustavo/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', 'gustavo.views.index', name='home'),
     url(r'^artigos/', include('artigos.urls')),
     url(r'^contato/', include('contatos.urls')),
+    url(r'^galerias/', include('galerias.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
